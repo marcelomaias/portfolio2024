4a1873daac30e26fd88ada83c0b7662d7bf5e2a2
Maia <marcelomaias@gmail.com>
2026-01-27 18:41:50 -0300

    2026 update

--- app/page.js
+++ app/page.js
@@ -12,12 +12,12 @@ import {
 } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
-import ContactForm from "@/components/ContactForm";
-import imgCooper from "@/app/images/coopernorte.coop.br.png";
-import imgOcean from "@/app/images/ocean-travel.vercel.app.png";
+import { SiPayloadcms } from "react-icons/si";
 import imgMueller from "@/app/images/shoppingmueller.com.br.png";
 import imgMilitec from "@/app/images/militecbrasil.com.br.png";
 import logo from "@/app/images/logo.svg";
+import imgDestinations from "@/app/images/destinations.jpg";
+import imgLandingPage from "@/app/images/payload-lp.jpg";
 
 export default function Home() {
   return (
@@ -42,13 +42,12 @@ export default function Home() {
             height={64}
           />
           <p>
-            Hello, I am Marcelo Maia, a web developer with a passion for
-            creating fast and user-friendly websites. With expertise in semantic{" "}
+            Hi, I’m Marcelo Maia, a web developer focused on creating fast,
+            intuitive, and well-crafted websites. I work with semantic{" "}
             <strong>HTML</strong>, <strong>CSS</strong>,{" "}
-            <strong>JavaScript</strong> and web technologies like{" "}
-            <strong>React</strong>, <strong>NextJS</strong>,{" "}
-            <strong>Tailwind</strong> and <strong>Wordpress</strong>, I have
-            built a diverse range of projects.
+            <strong>JavaScript</strong>, and tools like <strong>React</strong>,{" "}
+            <strong>Next.js</strong>, <strong>Tailwind</strong>, and{" "}
+            <strong>Payload CMS</strong> to bring ideas to life.
           </p>
 
           <div className="techs">
@@ -57,8 +56,8 @@ export default function Home() {
             <IoLogoJavascript size={64} color="#999" title="JavaScript" />{" "}
             <FaReact size={64} color="#999" title="React" />{" "}
             <RiNextjsFill size={64} color="#999" title="NextJS" />{" "}
+            <SiPayloadcms size={64} color="#999" title="Payload CMS" />
             <RiTailwindCssFill size={64} color="#999" title="Tailwind" />
-            <FaWordpress size={64} color="#999" title="Wordpress" />
           </div>
         </section>
 
@@ -69,9 +68,9 @@ export default function Home() {
               <div className="experience">
                 <h3>Front-end web developer | Self-Employed</h3>
                 <p>
-                  Currently working as a free-lancer doing mostly front-end
-                  development, but also with experience in backend using NodeJS,
-                  PHP, and with some CMSs like Wordpress.
+                  I currently work as a freelancer, focusing mainly on front-end
+                  development, while also having experience with back-end work
+                  using Payload CMS.
                 </p>
                 <strong>March 2024 - Present</strong>
               </div>
@@ -86,17 +85,18 @@ export default function Home() {
                   </Link>
                 </h3>
                 <p>
-                  I was part of a team responsible for developing the front-end
-                  part of dynamic websites built in PHP and Python.
+                  I was part of a team responsible for developing the front end
+                  of dynamic websites built with PHP and Python.
                 </p>
                 <strong>December 2019 - February 2024</strong>
               </div>
               <div className="experience">
                 <h3>Front-end web developer | Self-Employed</h3>
                 <p>
-                  Worked as a free-lancer doing mostly front-end development,
-                  but also had some experience in backend using NodeJS, PHP, and
-                  with some CMSs like Wordpress and Drupal.
+                  Worked as a freelancer, primarily focusing on front-end
+                  development, with additional experience in back-end
+                  development using Node.js and PHP, as well as CMS platforms
+                  such as WordPress and Drupal.
                 </p>
                 <strong>November 2006 - December 2018</strong>
               </div>
@@ -111,8 +111,8 @@ export default function Home() {
                   </Link>
                 </h3>
                 <p>
-                  I was part of a team responsible for developing the front-end
-                  part of dynamic websites built in PHP.
+                  I was part of a team responsible for developing the front end
+                  of dynamic websites built with PHP.
                 </p>
                 <strong>July 2001 - November 2004</strong>
               </div>
@@ -120,20 +120,24 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="container">
+        <section className="container demos">
           <h2>Demo</h2>
 
           <div className="card">
             <div className="description">
               <div>
                 <h3>
-                  <Link target="_blank" href="https://ocean-travel.vercel.app/">
-                    Ocean Travel <FaExternalLinkAlt color="#0075f2" size={16} />
+                  <Link
+                    target="_blank"
+                    href="https://destinations-three.vercel.app/"
+                  >
+                    Destinations <FaExternalLinkAlt color="#0075f2" size={16} />
                   </Link>
                 </h3>
                 <p>
-                  This demo showcases headless Wordpress with the front-end
-                  build with NextJS, for a very performant website.
+                  A website built with Payload showcasing some of the most
+                  common features of a modern website. Very performant, hosted
+                  serverless at Vercel.
                 </p>
               </div>
 
@@ -141,7 +145,7 @@ export default function Home() {
                 <Link
                   target="_blank"
                   className="btn code"
-                  href="https://github.com/marcelomaias/ocean-travel"
+                  href="https://github.com/marcelomaias/destinations"
                 >
                   <span>View code</span>{" "}
                   <FaExternalLinkAlt color="#fff" size={16} />
@@ -149,7 +153,7 @@ export default function Home() {
                 <Link
                   target="_blank"
                   className="btn"
-                  href="https://ocean-travel.vercel.app/"
+                  href="https://destinations-three.vercel.app/"
                 >
                   <span>View website</span>{" "}
                   <FaExternalLinkAlt color="#333" size={16} />
@@ -157,7 +161,50 @@ export default function Home() {
               </div>
             </div>
             <Image
-              src={imgOcean}
+              src={imgDestinations}
+              alt="Ocean Travel thumbnail"
+              className="thumbnail"
+            />
+          </div>
+
+          <div className="card">
+            <div className="description">
+              <div>
+                <h3>
+                  <Link
+                    target="_blank"
+                    href="https://landingpage-payload.vercel.app/"
+                  >
+                    Landing page <FaExternalLinkAlt color="#0075f2" size={16} />
+                  </Link>
+                </h3>
+                <p>
+                  A simple landing page featuring a very performant front and
+                  back-end, built with Payload.
+                </p>
+              </div>
+
+              <div className="actions">
+                <Link
+                  target="_blank"
+                  className="btn code"
+                  href="https://github.com/marcelomaias/landingpage-payload"
+                >
+                  <span>View code</span>{" "}
+                  <FaExternalLinkAlt color="#fff" size={16} />
+                </Link>
+                <Link
+                  target="_blank"
+                  className="btn"
+                  href="https://landingpage-payload.vercel.app/"
+                >
+                  <span>View website</span>{" "}
+                  <FaExternalLinkAlt color="#333" size={16} />
+                </Link>
+              </div>
+            </div>
+            <Image
+              src={imgLandingPage}
               alt="Ocean Travel thumbnail"
               className="thumbnail"
             />
@@ -181,7 +228,7 @@ export default function Home() {
             </h2>
 
             <div className="cards">
-              <div className="card">
+              {/* <div className="card">
                 <Image
                   src={imgCooper}
                   alt="Coopernorte thumbnail"
@@ -196,8 +243,8 @@ export default function Home() {
                       </Link>
                     </h3>
                     <span>
-                      Wordpress institutional website featuring a weather
-                      forecast API call.
+                      WordPress institutional website featuring a weather
+                      forecast API integration.
                     </span>
                   </div>
                   <div className="actions">
@@ -211,7 +258,7 @@ export default function Home() {
                     </Link>
                   </div>
                 </div>
-              </div>
+              </div> */}
               <div className="card">
                 <Image
                   src={imgMilitec}
@@ -230,8 +277,8 @@ export default function Home() {
                       </Link>
                     </h3>
                     <span>
-                      Wordpress institutional website featuring accessibility,
-                      custom filter and a full feature blog with social sharing.
+                      Institutional WordPress website featuring accessibility
+                      support and a fully featured blog with social sharing.
                     </span>
                   </div>
                   <div className="actions">
@@ -264,8 +311,9 @@ export default function Home() {
                       </Link>
                     </h3>
                     <span>
-                      Wordpress institutional website featuring accessibility
-                      and custom content types like Stores and Movies.
+                      Institutional WordPress website featuring accessibility
+                      support and custom content types such as Stores and
+                      Movies.
                     </span>
                   </div>
                   <div className="actions">
